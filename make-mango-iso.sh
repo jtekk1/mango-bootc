d115799e95e2734dc274ea757b4dcab07badc71f
jtekk <jtekk@jtekk.dev>
2026-05-19 10:31:46 -0600

    chore: changing kde back to kinoite and updating build scripts

--- make-mango-iso.sh
+++ make-mango-iso.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 sudo $(which bluebuild) generate-iso \
+  --variant server \
+  --tempdir /var/tmp \
   --iso-name nelhua-linux-mango.iso \
   image ghcr.io/jtekk1/nelhua-mango:latest
